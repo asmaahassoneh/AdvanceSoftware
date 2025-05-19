ecfd40a4be5cc697809a234642e00541921b7676
HalaKawni <hala.kawni@gmail.com>
2025-05-19 23:25:38 +0300

    Update sentimentService.js

--- services/sentimentService.js
+++ services/sentimentService.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') }); 
-console.log('API Key:', process.env.HUGGINGFACE_API_KEY); 
+
 
 const axios = require('axios'); 
 
