eafe44ad8e3f330b8332afac79ffe845929b5123
Asmaa <143655712+asmaahassoneh@users.noreply.github.com>
2025-05-20 19:29:09 +0300

    Delete routes/donorsRoutes.js

--- routes/donorsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getDonorDashboard } = require('../controllers/donorController');
-const { verifyToken } = require('../middleware/auth');
-
-// Authenticated route (any user, but front-end will call for donors)
-router.get('/dashboard',getDonorDashboard);
-
-module.exports = router;
